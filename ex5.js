3fc7ee57fcbcb4157c9b22fd3f03a66f699e1272
fataranto <fataranto@gmail.com>
2022-05-16 19:01:24 +0200

    ejercicio 5 finalizado

--- ex5.js
+++ ex5.js
@@ -3,12 +3,13 @@
  * Comrpruébalo con "node ex5.js"
  * ¿Sabrías decir porque?
  * 
- * ---------------------------------------------------
+ * No se ejecuta porque i tiene el mismo valor que veces, por lo tanto el bucle da por resultado false (5 no es menor que 5)
+ * 
  * Una vez lo averigues, arreglalo para que el bucle se ejecute exactamente 10 veces.  * Reestricción: SOLO puedes cambiar el valor de la variable 'veces' para conseguirlo
  */
 
- let veces = 5;
+ let veces = 15;
 
  for (let i=5; i<veces; i++) {
-     console.log("No me voy a ejecutar...")
+     console.log("No me voy a ejecutar..." + i)
  }
