05f2b9db5633085226fe03a4fe303eddc776bb49
fataranto <fataranto@gmail.com>
2022-05-16 19:17:43 +0200

    ejercicio 9 finalizado

--- ex9.js
+++ ex9.js
@@ -2,6 +2,7 @@
  * El bucle infinito
  *
  * El siguiente bucle 'while', no acaba nunca. ¿Por qué? Razona la respuesta.
+ * Es infinito porque no está modificando el valor de i, es decir que siempre comprueba que i (0) es menor que n (10)
  * 
  * Arreglalo para que la palabra "Ejecutar!" se muestre 10 veces por el terminal
  */
@@ -11,5 +12,6 @@ let i=0;
 
 while(i<n) {
     console.log("Ejecutar!")
+    i++;
 }
 
