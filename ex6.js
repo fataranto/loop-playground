014d6c034a22c80619f936cc7ee05ba9b9c29296
fataranto <fataranto@gmail.com>
2022-05-16 19:05:08 +0200

    ejercicio 56finalizado

--- ex6.js
+++ ex6.js
@@ -4,7 +4,7 @@
  * Ejecuta el script y observa que sucede.
  * ¿Cuantas veces se ejecuta el código del bucle?
  * 
- * Modifica el código del bucle y una esctrictura condicional if..else; para que:
+ * Modifica el código del bucle y una estructura condicional if..else; para que:
  * Si i es un número negativo, mostrar por el terminal "Soy un número negativo"
  * Si i es un número positivo, mostrar por el terminal "Soy un número positivo"
  */
@@ -14,8 +14,17 @@
  for (let i=-5;i<numeroFinal; i++) {
     console.log("Voy a ejecutar el condicional para el número: " + i)
     // Modificar a partir de aquí
-    console.log("Soy un número positivo!")
-    console.log("Soy un núemro negativo!")
+    if (i < 0 ) {
+        console.log("Soy un núemero negativo!")
+    } else {
+        if (i == 0) {
+            console.log("Soy un cero!")
+        } else {
+            console.log("Soy un número positivo!")
+        }
+    }
+    
+    
 }
 
 /**
